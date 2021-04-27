7e82782198a283c66a68e0b1e0d251171f765d8c
ericos <eric.schneider@netic.de>
2021-04-27 15:51:12 +0200

    Added install script

---
+++ install.lua
@@ -0,0 +1,26 @@
+--installation script
+--rhNbNbFE
+--Version 1.0
+--taken from https://github.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/blob/master/install.lua, credit goes to OmegaPhi115
+
+local libURL = "https://raw.githubusercontent.com/cricton/draconic-reactor-controller/main/lib/f.lua?token=AAHLOEFWJ5ZRT3ICNEZQFL3ARALI2"
+local startupURL = "https://raw.githubusercontent.com/cricton/draconic-reactor-controller/main/controller.lua?token=AAHLOEFSQOHXL4HIMFSAJ33ARALLG"
+local lib, startup
+local libFile, startupFile
+
+fs.makeDir("lib")
+
+lib = http.get(libURL)
+libFile = lib.readAll()
+
+local file1 = fs.open("lib/f", "w")
+file1.write(libFile)
+file1.close()
+
+startup = http.get(startupURL)
+startupFile = startup.readAll()
+
+local file2 = fs.open("startup", "w")
+file2.write(startupFile)
+file2.close()
+os.reboot()
